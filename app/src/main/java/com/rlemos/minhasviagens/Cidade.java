288aadae1496ab5aefde31d1ec9b2627ff1f0dd3
digojlemos <digojlemos@hotmail.com>
2017-07-29 11:28:59 -0300

    Inserções de detalhes de viagem Layou e no arquivo Cidade.java

--- app/src/main/java/com/rlemos/minhasviagens/Cidade.java
+++ app/src/main/java/com/rlemos/minhasviagens/Cidade.java
@@ -10,12 +10,26 @@ public class Cidade {
     private String mDescViagem;
     private String mEstado;
     private String mPais;
+    private String mQtdDias;
+    private String mLocalHospedagem;
+    private String mPasseios;
 
     public Cidade(String textCidade, String dtViagem, String estado,String pais){
         mTextCidade=textCidade;
         mDataChegada=dtViagem;
         mEstado=estado;
         mPais=pais;
+    }
+    public Cidade(String textCidade, String dtViagem, String qtdDias,String localHospedagem, String passeios,
+                  String detalhes, String estado, String pais){
+        mTextCidade=textCidade;
+        mDataChegada=dtViagem;
+        mEstado=estado;
+        mPais=pais;
+        mQtdDias = qtdDias;
+        mLocalHospedagem = localHospedagem;
+        mPasseios= passeios;
+        mDescViagem=detalhes;
     }
 
     public String getEstado(){
@@ -41,4 +55,27 @@ public class Cidade {
     public void setDescViagem(String descViagem){mDescViagem=descViagem;}
     public String getDesViagem () { return mDescViagem;}
 
+    public String getmQtdDias() {
+        return mQtdDias;
+    }
+
+    public void setmQtdDias(String qtdDias) {
+        mQtdDias = qtdDias;
+    }
+
+    public String getmLocalHospedagem() {
+        return mLocalHospedagem;
+    }
+
+    public void setmLocalHospedagem(String localHospedagem) {
+        mLocalHospedagem = localHospedagem;
+    }
+
+    public String getmPesseios() {
+        return mPasseios;
+    }
+
+    public void setmPesseios(String passeios) {
+        mPasseios = passeios;
+    }
 }
